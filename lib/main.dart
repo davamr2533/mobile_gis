5d3e678da84fd0c1c08a3326c9968519ca14f052
davamr2533 <davamr@students.amikom.ac.id>
2025-11-07 21:05:28 +0700

    notifikasi firebase pat 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart'; // hasil flutterfire configure
 import 'package:gis_mobile/splash_screen.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("📩 Notifikasi diterima di background: ${message.notification?.title}");
+}
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Init Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Background message handler
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Minta izin notifikasi
+  await FirebaseMessaging.instance.requestPermission();
+
+  // Ambil FCM Token
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("🔥 FCM TOKEN DEVICE INI: $token");
+
+  // Listener notif saat app dibuka (foreground)
+  FirebaseMessaging.onMessage.listen((message) {
+    print("📩 Notifikasi Foreground: ${message.notification?.title}");
+  });
+
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +47,7 @@ class MyApp extends StatelessWidget {
       title: 'Mobile GIS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        useMaterial3: false
+        useMaterial3: false,
       ),
       home: const SplashScreen(),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
+import firebase_core
+import firebase_messaging
 import flutter_image_compress_macos
 import geolocator_apple
 import path_provider_foundation
@@ -15,6 +17,8 @@ import shared_preferences_foundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
